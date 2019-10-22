45c2b5bf6a8f842f9df92bc7b11b13b9fa82db1d
Nate Scholnick <nate.scholnick@gmail.com>
2019-10-22 14:31:36 -0400

    Completed forms, TODO: add posting, profiles, database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
+
+# all variables based on this application must be instantiated after the app instance is created
+
 bootstrap = Bootstrap(app)
+
+# flask app instance uses from object method to laod in all configurartion variables
+app.config.from_object(Config)
 
 from app import routes
---
+++ app/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class TitleForm(FlaskForm):
+    title = StringField('Enter a new header:', validators=[DataRequired()])
+    submit = SubmitField('Change Title')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send Message')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    age = IntegerField('Age')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Re-Type Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
